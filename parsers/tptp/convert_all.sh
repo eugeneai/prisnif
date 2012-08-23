62a6c9bf171c7e09df0c7372815874cb146604c6
Evgeny Cherkashin <eugene@irnok.net>
2012-08-23 21:22:45 +0900

    Gather all the output into one file.

--- parsers/tptp/convert_all.sh
+++ parsers/tptp/convert_all.sh
@@ -5,7 +5,7 @@ outdir=tasks.out
 tmp=tmp_ast.txt
 pcf=../pcf
 log="conversion.log"
-
+out="conv_out.txt"
 
 q_tr="GLOBALSZ=150000 gprolog < tr_s.pl"
 
@@ -18,9 +18,10 @@ cd $outdir
 rm *
 cd - > /dev/null
 
-# prune the log file
+# prune the log file and output file.
 
 echo > $log
+echo > $out
 
 for file in $indir/*.p
 do
@@ -43,6 +44,10 @@ do
     if [ "$rc" = "0" ]; then
 	echo "Phase two is Ok $file" >> $log
 	mv $pcf/result.p $fo
+	echo "Soure file: $file" >> $out
+	echo "----------------------------------" >> $out
+	cat $pcf/gram_test.out >> $out
+	echo "==================================" >> $out
     fi
 
     rm -f $tmp $pcf/input.pl $pcf/result.p
