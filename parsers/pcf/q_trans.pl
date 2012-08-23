cfd85a23c4b23e36826aff82bb266b5392c03a7c
Evgeny Cherkashin <eugene@irnok.net>
2012-08-23 23:11:41 +0900

    Process disjunction during to PCF conversion.

--- parsers/pcf/q_trans.pl
+++ parsers/pcf/q_trans.pl
@@ -326,6 +326,8 @@ q_to_pcf_a_a(imp(conj(L),B), C, [FE]):-
 	q_split_conj(L, C, FF),
 	q_rd(disj([neg(conj(FF)),B]), F),
 	% XXX if F is a disjunction. It could be properly translated here
+	%write(F),nl,nl,
+        %q_to_pcf_a_a(F, _, FE).
         q_to_pcf_e(F, FE).
 
 q_to_pcf_a_a(imp(A,B), [], [F1, F2]):-
@@ -333,9 +335,10 @@ q_to_pcf_a_a(imp(A,B), [], [F1, F2]):-
         q_to_pcf_e(A1, F1),
         q_to_pcf_e(B, F2).
 
-q_to_pcf_a_a(disj([A,B]), [], [F1, F2]):-!,
-        q_to_pcf_e(A, F1),
-        q_to_pcf_e(B, F2).
+q_to_pcf_a_a(disj([]), [t('False')], []):-!.
+q_to_pcf_a_a(disj([X|T]), [], [FX|TF]):-!,
+	q_to_pcf_e(X,FX),
+	q_to_pcf_a_a(disj(T), _, TF).
 
 q_to_pcf_a_a(neg(A), [A], [F]):-!,
         q_to_pcf_e(t('False'), F).
