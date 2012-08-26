50d34a903bcba40bffca8d761b376a30e1510778
Evgeny Cherkashin <eugene@irnok.net>
2012-08-26 14:02:44 +0900

    Switched to SWI prolog. It converted some more tasks.

--- parsers/pcf/tptp_tr.sh
+++ parsers/pcf/tptp_tr.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-GLOBALSZ=400000 gprolog < tr_s.pl | tee gram_test.out 
+swipl -s q_trans.pl -t halt -nosignals -L1300000 | tee gram_test.out 
---
+++ parsers/pcf/tptp_tr_gpro.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+GLOBALSZ=400000 gprolog < tr_s.pl | tee gram_test.out 
