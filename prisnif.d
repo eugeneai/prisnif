05470fb7d43a114092bb057fca5ab354c950158a
alexander <ale@prisnif.org>
2012-09-07 19:44:51 +0900

    Сохранил в диссере инфу о НЕКОТОРЫХ решенных задачах.

--- prisnif.d
+++ prisnif.d
@@ -33,16 +33,50 @@ class Prover{
 		writeln("================================");
 	}
 
+	//Адекватный размер формулы, без равенств, без неограниченных переменных
 	void uncoTest(){
-		foreach (string name; dirEntries("../tasks.out/3", SpanMode.breadth)) { 
-			string s = readText(name);
-			if(indexOf(s,'=')<0){
-				//writeln(name);
-				ParserHu ph = new ParserHu();
-				ph.parseFromFile(name);
-				if(!ph.unco()) writeln(name, ": Ok.");
-				//writeln(name, ": Ok.");
+		foreach (string name; dirEntries("/home/ale/Dropbox/TPTP/", SpanMode.breadth)) { 
+			auto size = getSize(name);
+			//Размер от 20 байт до 1000 000 байт
+			if((size>20) && size<1000000){			
+				string s = readText(name);
+				if(indexOf(s,'=')<0){
+					//writeln(name);
+					ParserHu ph = new ParserHu();
+					ph.parseFromFile(name);
+					if(!ph.unco()) writeln(name);
+					//writeln(name, ": Ok.");
+				}
 			}
+		}
+	}
+
+	void copyx(){
+		foreach (string name; dirEntries("/home/ale/Dropbox/TPTP/", SpanMode.breadth)) { 
+			//string s = readText(name);
+			auto size = getSize(name);
+			if((size>20) && size<100000){
+				writeln(name,": ",size);
+				//copy(name,"../tasks.out/BIG/"~name);
+			} 
+		}
+	}
+
+	//адекватный размер, без равенств. Возможны неограниченные перменные
+	void select(){
+		foreach (string name; dirEntries("/home/ale/Dropbox/TPTP/", SpanMode.breadth)) { 
+			auto size = getSize(name);
+			//Размер от 20 байт до 
+			if((size>20) && size<10000){
+				string s = readText(name);
+				//без равенства
+				if(indexOf(s,'=')<0){
+					//ParserHu ph = new ParserHu();
+					//ph.parseFromFile(name);
+					//if(!ph.unco()) writeln(name, ": Ok.");
+					writeln(name);
+				}
+			} 			
 		}
 	}
 	
@@ -52,9 +86,13 @@ class Prover{
 /*================ MAIN ================*/
 void main(string args[]){
 	Prover p = new Prover();
-	if (args[1]=="z"){
+	if (args[1]=="u"){
 		p.uncoTest();
-	} else{
+	}else if(args[1]=="c"){
+		p.copyx();
+	} else if(args[1]=="s"){
+		p.select();
+	}else{
 		p.start(args[1],to!int(args[2]));
 	}
 
