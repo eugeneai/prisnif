9e7a6c21669d823ea858614aa711455e067f17dd
Evgeny Cherkashin <eugeneai@irnok.net>
2012-11-08 14:03:05 +0900

    Refined Makefile. Refined first paragraph in autoref.

--- parsers/pcf/tptp_tr_gpro.sh
+++ parsers/pcf/tptp_tr_gpro.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #echo "------ GLOBALSZ=400000 ./q_trans --out $2 --tptp $1"
-GLOBALSZ=400000 TRAILSZ=400000 ./q_trans --out $2 --tptp $1 | tee gram_test.out
+GLOBALSZ=400000 LOCALSZ=400000 TRAILSZ=400000 ./q_trans --out $2 --tptp $1 | tee gram_test.out
